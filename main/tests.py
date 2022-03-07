4f551e2700946ac5bebf3690fa61bfad317f998a
Maribel Ramos <maribelrb.5@gmail.com>
2022-03-07 11:40:20 +0100

    Add tests to check endpoint
    Co-Authored-By: Javier Vilariño Mayo <44314228+javivm17@users.noreply.github.com>

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+class TestCheckingMessage(APITestCase):
+
+    def test_check_phishing_message(self):
+        url = "/check"
+        data = {'message': 'Este es el mensaje que he recibido https://testsafebrowsing.appspot.com/s/phishing.html'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.json()["result"])
+
+    def test_check_not_phishing_message(self):
+        url = "/check"
+        data = {'message': 'Este es el mensaje que he recibido https://google.com'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.json()["result"])
+
+    def test_check_message_without_url(self):
+        url = "/check"
+        data = {'message': 'Este es el mensaje que he recibido sin URL'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, 400)
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.status import (
     HTTP_200_OK,
+    HTTP_400_BAD_REQUEST
 )
 from . import serializers
 from django.views.decorators.csrf import csrf_exempt
@@ -16,11 +17,15 @@ class check_url_blacklist(generics.CreateAPIView):
     def post(self, request):
         body = request.data
         message = body['message']
-        url = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message)[0]
-        print(url)
-        result = googleapi.check_url(url)
-        return Response(
-            data={"result": ("Este sitio web parece ser seguro", "Este sitio web no es seguro")[result]}, 
-            status=HTTP_200_OK
-        )
-
+        url = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message)
+        if len(url) > 0:
+            url = url[0]
+            result = googleapi.check_url(url)
+            return Response(
+                data={"result": (False, True)[result]}, 
+                status=HTTP_200_OK
+            )
+        else:
+            return Response( 
+                status=HTTP_400_BAD_REQUEST
+            )
