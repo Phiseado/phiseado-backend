9d0eb337c2fada11f8549ad5e5bd30ee6ef6cda2
javivm17 <javiervilarinyomayo.cr@gmail.com>
2022-03-20 12:52:35 +0100

    Creates database for saving messages
    Co-Authored-By: Maribel Ramos <62081471+maribelrb22@users.noreply.github.com>

--- main/models.py
+++ main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    url = models.URLField()
+    registered_date = models.DateTimeField(auto_now_add=True)
+    considered_phishing = models.BooleanField(default=False)    
+
+    domain = models.ForeignKey('Domain', on_delete=models.CASCADE)
+    country = models.ForeignKey('Country', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.url + " " + str(self.registered_date)
+
+class Domain(models.Model):
+    name = models.CharField(max_length=255)
+    frequency = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name + " " + str(self.frequency)
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+    phone_prefix = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name + " " + self.phone_prefix
